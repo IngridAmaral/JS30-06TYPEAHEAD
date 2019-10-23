0b9eb47086285a5b9796fb0a33538c1c036f1c46
IngridAmaral <52465213+IngridAmaral@users.noreply.github.com>
2019-10-23 16:08:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,35 @@ fetch(endpoint) /*fetch returns a promise*/
     .then(blob => blob.json()) /*returned data from fetch and converts to a json that will be used next*/
     .then(data => cities.push(...data)) /*creates an array with all the cities using the previous promise converted*/
 
-function findMatches(wordMatch, cities){
+function findMatches(wordMatch, cities) {
     return cities.filter(place => {
         const regex = new RegExp(wordMatch, 'gi');
         return place.city.match(regex) || place.state.match(regex)
-    })
+    });
 }
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities)
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi')
+        const cityName =  place.city.replace(regex, `<span class="hl">${this.value}</span>`)
+        const stateName =  place.state.replace(regex, `<span class="hl">${this.value}</span>`)
+        return `
+        <li>
+            <span class="name">${cityName}, ${stateName}</span>
+            <span class="population">${numberWithCommas(place.population)}</span>
+        </li>
+        `;
+    }).join('')
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches)
+searchInput.addEventListener('keyup', displayMatches)
